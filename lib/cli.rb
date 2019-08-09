6d1ef5b06483541eee09a447458a0c7ca568efd1
Oscar Shoudt <shoudto@gmail.com>
2019-08-09 01:12:07 +0000

    adding color

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,14 +2,13 @@ class Cli
 
   def start
 
-    # => Introduction of the code.
+    # => Introduction
     puts " "
-    puts "Welcome to the Top List Craft Beers!!!"
-    puts "--------------------------------------"
+    puts "Welcome to the Top List Craft Beers!!!".colorize(:color => :green)
+    puts "--------------------------------------".colorize(:color => :green)
     puts " "
-    #---------------------------------------------
-
-    # => Adding a list of beers from my scraper class
+    #-------------------------------------------------------------------------------
+    # calling instance and class method
     Scraper.scrape_beer_list
     print_beers
     pick_your_craft
@@ -20,40 +19,42 @@ class Cli
     input = ""
     while input != "exit" do
 
-        puts "Choose your BEER!"
-        puts "Enter a number or type 'exit' to exit."
+        puts " "
+        puts "Choose your BEER!".colorize(:color => :green)
+        puts "Enter a number or type 'exit' to exit.".colorize(:color => :green)
         puts " "
 
         input = gets.strip.downcase
 
+        # the beers attributes and info
         if (1..Beer.all.count).include?(input.to_i)
           Scraper.scrape_beer_info(Beer.all[input.to_i-1]) if !Beer.all[input.to_i-1].style
 
           puts " "
-          puts "Name: #{Beer.all[input.to_i-1].name}"
+          puts "Name: #{Beer.all[input.to_i-1].name}".colorize(:color => :green)
           puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-          puts "Style: #{Beer.all[input.to_i-1].style}"
+          puts "Style: #{Beer.all[input.to_i-1].style}".colorize(:color => :green)
           puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-          puts "Company:#{Beer.all[input.to_i-1].company}"
+          puts "Company:#{Beer.all[input.to_i-1].company}".colorize(:color => :green)
           puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-          puts "ABV: #{Beer.all[input.to_i-1].abv}"
+          puts "ABV: #{Beer.all[input.to_i-1].abv}".colorize(:color => :green)
           puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-          puts "Location: #{Beer.all[input.to_i-1].location}"
+          puts "Location: #{Beer.all[input.to_i-1].location}".colorize(:color => :green)
           puts " "
-
+        #-------------------------------------------------------------------------------
+        # error message to try again.
         elsif input != "exit"
-
           puts " "
-          puts "ERROR!"
+          puts "INCORRECT ENTRY! Please try again".colorize(:color => :white, :background => :red)
           puts " "
-
+        #-------------------------------------------------------------------------------
         end
       end
   end
 
   def print_beers
     Beer.all.each.with_index(1) do |beer, index|
-      puts "#{index}. #{beer.name}"
+      puts "#{index}. #{beer.name}".colorize(:color => :red)
     end
   end
 end
