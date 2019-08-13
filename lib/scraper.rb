ea68b4470196e4cee5a12df8514b4b3ef72d1015
Oscar Shoudt <shoudto@gmail.com>
2019-08-13 17:30:36 +0000

    fixing styles font

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
 
 
       # => iteration to scrape the name of the beer and url of the beer
-      
+
       beer_list.drop(1).each do |beer|
         brew = Beer.new
         brew.name = beer.css("a b").text
@@ -26,10 +26,9 @@ class Scraper
     html = open("https://www.beeradvocate.com#{beer.url}")
     doc = Nokogiri::HTML(html)
 
-    beer.style =  doc.css("a b").text
+    beer.style =   doc.css("dd[class=beerstats] a b").text
     beer.abv = doc.css("dd[class=beerstats]")[1].text
     beer.company = doc.css("dd[class=beerstats]")[6].text
     beer.location = doc.css("dd[class=beerstats]")[7].text
-
   end
 end
